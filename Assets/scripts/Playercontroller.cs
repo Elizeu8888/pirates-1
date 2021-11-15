6b039897792c5ce9692971cf539b369f6503728b
Elizeu8888 <527603@chrome.escg.ac.uk>
2021-11-15 00:01:59 +0000

    fixed grounded and naimation and health

--- Assets/scripts/Playercontroller.cs
+++ Assets/scripts/Playercontroller.cs
@@ -7,8 +7,13 @@ public class Playercontroller : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
     public float speed = 6f;
-    public float turnsmoothing = 0.1f;
+    public float turnsmoothing = 0.1f;// for movement
     float turnsmoothvelocity = 0.5f;
+
+    public Healthbar healthbar;
+    public int maxHealth = 100;// health
+    public int currentHealth;
+
 
 
     public static int playerscore = 0;
@@ -17,33 +22,83 @@ public class Playercontroller : MonoBehaviour
     GameObject ground;
 
     public float gravity = -9.81f;
-    public float jumpheight = 5f;
+    public float jumpheight = 5f;// for jumping
 
     public Transform groundcheck;
-    public float grounddistance = 0.4f;
+    public float grounddistance = 0.4f;// for is grounded
     public LayerMask groundmask;
 
     public bool isgrounded;
     Vector3 velocity;
+
     public Animator anim;
 
     void start()
     {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
         ground = GameObject.FindGameObjectWithTag("ground");
         coin = GameObject.FindGameObjectWithTag("Coin");
         anim.SetBool("weapondrawn", false);
-        //anim = GetComponent<Animator>();
+
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthbar.SetHealth(currentHealth);
+
     }
 
 
     void Update()
     {
+        
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");// uses imput to find direction
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+
+
+        velocity.y += gravity * Time.deltaTime;// here you fall faster the longer you fall
+        controller.Move(velocity * Time.deltaTime);
+
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+
+            float targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;// finds direction of movement
+            
+            
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnsmoothvelocity, turnsmoothing);// makes it so the player faces its movement direction
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);// makes it so the player faces its movement direction
+
+
+            Vector3 movedir = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;// here is the movement
+            controller.Move(movedir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+
         anim.SetLayerWeight(1, 0);
 
         anim.SetBool("jump", false);
 
         if (Input.GetMouseButton(0))
         {
+
+
+
+
+
             anim.SetLayerWeight(1, 1);
         }
         else
@@ -56,11 +111,6 @@ public class Playercontroller : MonoBehaviour
 
         anim.SetBool("grounded", false);
 
-        //isgrounded = Physics.CheckSphere(groundcheck.position, grounddistance, groundmask);
-
-
-
-
 
         if (isgrounded && velocity.y < 0)
         {
@@ -69,40 +119,11 @@ public class Playercontroller : MonoBehaviour
 
 
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-
-
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-
-
-
-        if (direction.magnitude >= 0.1f)
-        {
-
-
-            float targetangle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetangle, ref turnsmoothvelocity, turnsmoothing);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            
-            Vector3 movedir = Quaternion.Euler(0f, targetangle, 0f) * Vector3.forward;
-            controller.Move(movedir.normalized * speed * Time.deltaTime);
-        }
-        else
-        {
-            GetComponent<Rigidbody>().velocity = Vector3.zero;
-        }
 
 
         if (Input.GetKey("space") && isgrounded)
         {
-            //anim.SetBool("jump", true);
             velocity.y = Mathf.Sqrt(jumpheight * -2f * gravity);
         }
 
@@ -118,9 +139,6 @@ public class Playercontroller : MonoBehaviour
 
         anim.SetBool("walking", false);
 
-
-
-
         if ((GetComponent<Rigidbody>().velocity.magnitude > 0.1) && isgrounded)
         {
             anim.SetBool("walking", true);
@@ -134,16 +152,17 @@ public class Playercontroller : MonoBehaviour
 
 
 
-
-
-
-
-
-
-
     }
 
-    void OnTriggerEnter(Collider other)
+    void FixedUpdate()
+    {
+        isgrounded = Physics.CheckSphere(groundcheck.position, grounddistance, groundmask);
+    }
+
+
+
+
+    void OnTriggerStay(Collider other)
     {
         print("ontrigger=" + other.gameObject.tag);
 
@@ -152,14 +171,14 @@ public class Playercontroller : MonoBehaviour
             playerscore = playerscore + 420;
         }
 
-        if (other.gameObject.tag == "ground")
+        
+        if (other.gameObject.tag == "enemy")
         {
-            isgrounded = true;
+            TakeDamage(5);
         }
-        else
-        {
-            isgrounded = false;
-        }
+        
+
+
 
     }
 
@@ -167,19 +186,14 @@ public class Playercontroller : MonoBehaviour
 
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "ground")
+        /*if (other.gameObject.tag == "ground")
         {
             isgrounded = false;
         }
         else
         {
             isgrounded = true;
-        }
+        }*/
     }
 
-    void FixedUpdate()
-    {
-
-
-    }
 }
