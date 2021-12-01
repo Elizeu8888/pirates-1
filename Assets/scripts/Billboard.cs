5df058184c0cc329bc0ddefe6213fc7f746afccd
Elizeu8888 <527603@chrome.escg.ac.uk>
2021-12-01 14:15:41 +0000

    enemy health and attempted weapon drawing

---
+++ Assets/scripts/Billboard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform cam;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void LateUpdate()
+    {
+        transform.LookAt(cam.position);
+    }
+}
---
+++ Assets/scripts/Weapon.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform hand;
+    public Transform back;
+
+    bool weaponout;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponout = false;
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey("f") && weaponout == false)
+        {
+            print("ddd");
+            transform.SetParent(hand);
+
+
+            weaponout = true;
+        }
+        else
+        {
+            weaponout = false;
+        }
+        if (weaponout == true)
+        {
+            
+        }
+
+
+
+        if (Input.GetKey("f") && weaponout == true)
+        {
+
+            print("ddd");
+
+            transform.SetParent(back);
+
+            weaponout = false;
+        }
+        else
+        {
+            weaponout = true;
+        }
+        
+        if (weaponout == false)
+        {
+           
+        }
+
+
+    }
+
+
+
+
+}
